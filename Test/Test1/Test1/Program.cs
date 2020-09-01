1c8a99d1eacf11fbf21ba6e460c2ab34a441f7ff
SARA\isak0 <isak098@mail.com>
2020-09-01 10:52:49 +0200

    Initial commit

---
+++ Test/Test1/Test1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+//Int, double, char, string, bool
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[,] chessBoard = new int[8, 8];
+
+            int width = 128;
+            int height = 128;
+
+            byte[,] blackWhiteImage = new byte[128, 128];
+            
+            for (var y = 0; y < height; y++)
+            {
+                Console.WriteLine(y);
+                for (var x = 0; x < width; x++)
+                {
+                    byte currentPixelValue = blackWhiteImage[x, y];
+                    Console.Write(x);
+                    Console.Write(' ');
+                    
+                }
+            }
+            Console.WriteLine(blackWhiteImage);
+
+            //bool inputIsNullOrEmpty = string.IsNullOrEmpty(inputData);
+
+            //bool inputIsNullOrWhiteSpace = string.IsNullOrWhiteSpace(inputData);
+            //Trimma och ta bort mellanslag
+            //string trimmedSentence = sentence.Trim();
+
+            //Det går att ange ett speciellt tecken som skall tas bort
+            //string.punctuationTrimmedSentence = endTrimmedSentence.TrimEnd('.');
+
+            //string.searchForWord = "text";
+            //int indexOfText = trimmedSentence.IndexOf(searchForWord);
+
+        }
+        
+    }
+}
